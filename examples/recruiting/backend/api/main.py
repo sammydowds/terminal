00b2c144bc8b705dad4e7c19f07e640f36085cd3
vik <sammydowds1993@gmail.com>
2024-12-05 09:23:27 -0600

    chore: tweak api and add frontend

--- examples/recruiting/backend/api/main.py
+++ examples/recruiting/backend/api/main.py
@@ -1,19 +1,35 @@
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 from .processor import ResumeProcessor
+from pydantic import BaseModel, Field
+from typing import List
 
 app = FastAPI()
 processor = ResumeProcessor()
 
-@app.get("/api/chat")
-async def root(q: str = "", stream: bool = False):
-    content = processor.retrieve_content(q)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to specify allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-    if not q:
-        return {"error": "Query parameter 'q' is required."}, 400
-    
-    if stream:
-        return StreamingResponse(processor.stream_completion(content, q), media_type="text/event-stream")
-    else:
-        response = processor.completion(content, q)
-        return { "message": response }
+class Message(BaseModel):
+    content: str
+
+class Payload(BaseModel):
+    messages: List[Message] = Field(..., description="List of messages")
+
+@app.post("/api/chat")
+async def chat(payload: Payload):
+    print(payload)
+    messages = payload.messages
+    query = messages[-1].content
+
+    if not query:
+        return {"error": "Missing 'query' is required."}, 400
+
+    content = processor.retrieve_content(query)
+    return StreamingResponse(processor.stream_completion(content, query), media_type="text/event-stream")
---
+++ examples/recruiting/frontend/app/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useChat } from "ai/react";
+
+export default function Home() {
+  const {input, handleInputChange, handleSubmit, messages } = useChat({ api: process.env.NEXT_PUBLIC_CHAT_API, streamProtocol: 'text'})
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            {messages.map(m => (
+        <div key={m.id} className="whitespace-pre-wrap">
+          {m.role === 'user' ? 'User: ' : 'AI: '}
+          {m.content}
+        </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
+  );
+}
